a2559bc6abde2865cd4c93a6cb164c98f27dbde9
SurajSonawane04 <sonawanesuraj1410@gmail.com>
2026-08-18 13:57:37 +0530

    first commit

--- tests/test_api.py
+++ tests/test_api.py
@@ -11,7 +11,7 @@ sys.path.insert(
 
 from app import app
 def test_home():
-    client = app.test_cliet()
+    client = app.test_client()
     response = client.get("/")
     assert response.status_code == 200
     data = response.get_json()
